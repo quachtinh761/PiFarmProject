6f43258be92bafdef15d10da366a314a0cc9fc3e
Tươi Hoàng Minh <Ti Hong Minh>
2016-11-07 13:43:42 +0700

    AutoLogin
    Edit file MainActivity to auto Login.

--- sourceCode/app/src/main/java/vn/com/gant/pifarm/Login.java
+++ sourceCode/app/src/main/java/vn/com/gant/pifarm/Login.java
@@ -1,7 +1,9 @@
 package vn.com.gant.pifarm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -22,6 +24,27 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        
+        Log.i(TAG, "onCreate: done");
+
+        // attach to layout element
+        etxtEmail = (EditText)findViewById(R.id.edtMail);
+        etxtPass = (EditText)findViewById(R.id.edtPassword);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        ibtnFacebook = (ImageButton)findViewById(R.id.ibtnFacebook);
+        ibtnGoogle = (ImageButton)findViewById(R.id.ibtnGoogle);
+        txvForgotPass = (TextView)findViewById(R.id.txvForgotPass);
+
+        // set manager for element
+        etxtEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick: done");
+            }
+        });
+    }
+
+    public void onDestroy(){
+        super.onDestroy();
+        Log.i(TAG,"destroy");
     }
 }
--- sourceCode/app/src/main/java/vn/com/gant/pifarm/MainActivity.java
+++ sourceCode/app/src/main/java/vn/com/gant/pifarm/MainActivity.java
@@ -1,14 +1,37 @@
 package vn.com.gant.pifarm;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginMessages";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+//        setContentView(R.layout.activity_login);
+
+        Log.i(TAG,"user name:");
+
+        if(SaveSharedPreference.getUserName(MainActivity.this).length() == 0){
+            // create Intent
+            Intent mainIntent = new Intent(MainActivity.this, Login.class);
+
+            MainActivity.this.startActivity(mainIntent);
+
+            finish();
+        } else {
+            // create Intent
+            Intent mainIntent = new Intent(MainActivity.this, Features.class);
+
+            MainActivity.this.startActivity(mainIntent);
+
+            finish();
+        }
+
 
     }
 }
