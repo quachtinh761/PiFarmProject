6f0f17acec76304d5c27770d7d69ac2cde45f05f
Tươi Hoàng Minh <Ti Hong Minh>
2016-11-07 07:25:41 +0700

    Edit layout Login
    Edit layout Login to fit with screen size 1920x1080px

--- sourceCode/app/src/main/java/vn/com/gant/pifarm/Login.java
+++ sourceCode/app/src/main/java/vn/com/gant/pifarm/Login.java
@@ -2,8 +2,20 @@ package vn.com.gant.pifarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    public static final String TAG = "LoginMessages";
+    EditText etxtEmail;
+    EditText etxtPass;
+    Button btnLogin;
+    TextView txvForgotPass;
+    ImageButton ibtnFacebook;
+    ImageButton ibtnGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
