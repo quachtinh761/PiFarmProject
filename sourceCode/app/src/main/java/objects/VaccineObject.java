79145b4e6e275625d08b3fe10ae8538cbd9dc28d
hongnhan <hongnhan100@gmail.com>
2016-11-12 05:52:07 +0700

    add insertedDate,updatedDate,inseredBy,updatedBy into VaccineObject and Create VaccineModel

--- sourceCode/app/src/main/java/objects/VaccineObject.java
+++ sourceCode/app/src/main/java/objects/VaccineObject.java
@@ -1,17 +1,24 @@
 package objects;
 
+import java.util.Date;
+
 /**
  * Created by vanthi on 11/4/2016.
  */
 
 public class VaccineObject {
-    String ID,name,indication,dose;
+    String ID,name,indication,dose,insertedBy,updatedBy;
+    Date insertedDate,updatedDate;
 
-    public VaccineObject(String ID, String name, String indication, String dose) {
+    public VaccineObject(String ID, String name, String indication, String dose, String insertedBy, String updatedBy, Date insertedDate, Date updatedDate) {
         this.ID = ID;
         this.name = name;
         this.indication = indication;
         this.dose = dose;
+        this.insertedBy = insertedBy;
+        this.updatedBy = updatedBy;
+        this.insertedDate = insertedDate;
+        this.updatedDate = updatedDate;
     }
 
     public String getID() {
@@ -44,5 +51,37 @@ public class VaccineObject {
 
     public void setDose(String dose) {
         this.dose = dose;
+    }
+
+    public String getInsertedBy() {
+        return insertedBy;
+    }
+
+    public void setInsertedBy(String insertedBy) {
+        this.insertedBy = insertedBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public Date getInsertedDate() {
+        return insertedDate;
+    }
+
+    public void setInsertedDate(Date insertedDate) {
+        this.insertedDate = insertedDate;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
     }
 }
